e314610d1aae59499a56c088fd89f8f26e747214
BugHunter-X <askzenai@gmail.com>
2025-02-15 05:42:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,25 @@
+```javascript
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+function MyComponent() {
+  const router = useRouter();
+
+  const navigateToHome = () => {
+    router.push('/');
+  };
+
+  const navigateToAbout = () => {
+    router.push('/about');
+  };
+
+  return (
+    <div>
+      <button onClick={navigateToHome}>Home</button>
+      <button onClick={navigateToAbout}>About</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
